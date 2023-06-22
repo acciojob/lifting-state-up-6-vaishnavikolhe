bd47c827bf0635edbd247e3b5196ecc465f78762
vaishnavikolhe <vaishnavikolhe1810@gmail.com>
2023-06-23 00:19:49 +0530

    s

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import Child from './Child';
-const Parent = () => {
-    let [todos,setdotos]=useState({
-        'Learn React':'pending',
-        'Buid a React app':'pending',
-        'Deploy the React app':'pending',
-    })
-    return (
-        <div>
-            <h1>Parent Component</h1>
-            <Child 
-                todos={todos}
-                setdotos={setdotos}
-            />
-        </div>
-    )
-}
+// import React, { useState } from 'react';
+// import Child from './Child';
+// const Parent = () => {
+//     let [todos,setdotos]=useState({
+//         'Learn React':'pending',
+//         'Buid a React app':'pending',
+//         'Deploy the React app':'pending',
+//     })
+//     return (
+//         <div>
+//             <h1>Parent Component</h1>
+//             <Child 
+//                 todos={todos}
+//                 setdotos={setdotos}
+//             />
+//         </div>
+//     )
+// }
 
-export default Parent
+// export default Parent
